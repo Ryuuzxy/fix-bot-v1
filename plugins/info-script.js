2ad38c8f763a74022e5673e3ba8b1ef54af3f0cd
Ryuuzxy <109670909+Ryuuzxy@users.noreply.github.com>
2022-09-13 07:03:29 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*https://www.youtube.com/channel/UCBYk6B1tkZ233ElCqImkC7Q*
+
+Mau script?
+
+Sorry script ini privat, tidak untuk disebar😅🙏
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://instagram.com/raffuri",
+    mediaType: "VIDEO",
+    description: "https://instagram.com/raffuri", 
+    title: '*タRyυυzхyツ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
